77e6efe8014f46efebcae544ac67a0a7f82b7650
Abhishek <abhimudda2003@gmail.com>
2025-03-14 16:07:58 +0530

    change made to sum file take user input and included function

--- sum.py
+++ sum.py
@@ -1,11 +1,19 @@
-sum=0
-for i in range(10):
-    sum=sum+i
-print(sum)
+def cal_sum(num):
+    sum=0
 
+    for i in range(num):
+        sum=sum+i
 
-fact=1
-for i in range (1,4):
-    fact=fact * i
+    return sum
 
-print(fact)
+def cal_fact(num):
+    fact=1
+
+    for i in range (1,num):
+        fact=fact * i
+
+    return fact
+
+num=int(input("enter the number"))
+print(cal_sum(num))
+print(cal_fact(num))
